00b041f485bf01527bffb8f077ef34dd3d96e319
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-05-08 20:09:29 +0300

    added fonts, images and basic scss file sctructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-import { useState } from 'react';
-import './App.scss';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Gadgets Store</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          {`likes: ${count}`}
-        </button>
-      </div>
     </>
   );
 }
