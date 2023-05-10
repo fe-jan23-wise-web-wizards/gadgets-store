759ac30f97968e215f6e5666c1e6e0f41df11fd2
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-05-10 13:36:13 +0300

    added axios and react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "./utils/router";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './utils/router';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   );
 }
 
