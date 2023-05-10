6e08820b63ed01ca72d11055edbc61778ee25794
Vladyslav Mosor <mosorvlad@gmail.com>
2023-05-10 14:18:31 +0200

    not found page created + extends added + vars added

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,36 @@
+import { Link } from 'react-router-dom';
+import styles from './NotFoundPage.module.scss';
+
+import errorImage from '@/assets/error-image.svg';
+
 export const NotFoundPage = () => {
   return (
-    <div>
-      <h1>Not found</h1>
+    <div className={styles.page}>
+      <div className={styles.page_message}>
+        <p className={styles.page_message_code}>
+          404
+        </p>
+
+        <p className={styles.page_message_status}>
+          Page not found
+        </p>
+
+        <p className={styles.page_message_description}>
+          You've clicked on a bad link or entered an invalid URL
+        </p>
+
+
+        <Link className={styles.page_message_homelink} to='/'>
+          Go back to home page
+        </Link>
+
+      </div>
+
+      <img
+        className={styles.page_image}
+        src={errorImage}
+        alt="Error 404 image"
+      />
     </div>
   );
 };
