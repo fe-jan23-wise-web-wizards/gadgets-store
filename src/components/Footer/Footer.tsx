2db9e2b905f06ae4b7acd0dcef4d1682cd37919c
andrej <andersontwentytwo@gmail.com>
2023-05-10 19:51:48 +0300

    created separate components for links in Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,21 +1,11 @@
-import backToTop from '@/assets/icons/icon-backToTop.svg';
+import { BackToTopLink } from '@/components/Footer/BackToTopLink/BackToTopLink';
+import { FooterLink } from '@/components/Footer/FooterLink/FooterLink';
 import { Logo } from '@/components/Logo/Logo';
 import mainLayoutStyles from '@/layouts/MainLayout/MainLayout.module.scss';
-import * as classNames from 'classnames';
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import styles from './Footer.module.scss';
 
 export const Footer: FC = () => {
-  const [isBTTHovered, setIsBTTHovered] = useState(false);
-
-  const handleMouseEnter = () => {
-    setIsBTTHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsBTTHovered(false);
-  };
-
   return (
     <footer className={styles.footer}>
       <div className={mainLayoutStyles.container}>
@@ -25,65 +15,23 @@ export const Footer: FC = () => {
           </div>
 
           <ul className={styles.footer_links_list}>
-            <li className={styles.footer_links_item}>
-              <a
-                href="https://github.com/fe-jan23-wise-web-wizards"
-                target="_blank"
-                rel="noreferrer"
-                className={styles.footer_links_list_link}
-              >
-                GitHub
-              </a>
-            </li>
+            <FooterLink
+              href={'https://github.com/fe-jan23-wise-web-wizards'}
+              content={'GitHub'}
+            />
 
-            <li className={styles.footer_links_item}>
-              <a
-                href="https://github.com/fe-jan23-wise-web-wizards"
-                target="_blank"
-                rel="noreferrer"
-                className={styles.footer_links_list_link}
-              >
-                Contacts
-              </a>
-            </li>
+            <FooterLink
+              href={'https://github.com/fe-jan23-wise-web-wizards'}
+              content={'Contacts'}
+            />
 
-            <li className={styles.footer_links_list_item}>
-              <a
-                href="https://github.com/fe-jan23-wise-web-wizards"
-                target="_blank"
-                rel="noreferrer"
-                className={styles.footer_links_list_link}
-              >
-                Rights
-              </a>
-            </li>
+            <FooterLink
+              href={'https://github.com/fe-jan23-wise-web-wizards'}
+              content={'Rights'}
+            />
           </ul>
 
-          <a
-            className={styles.footer_nav}
-            onClick={() =>
-              window.scrollTo({
-                top: 0,
-                behavior: 'smooth',
-              })
-            }
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
-            <p className={styles.footer_nav_text}>Back to top</p>
-
-            <div
-              className={classNames(styles.footer_nav_icon_container, {
-                [styles.footer_nav_icon_container_active]: isBTTHovered,
-              })}
-            >
-              <img
-                className={styles.footer_nav_icon}
-                src={backToTop}
-                alt="BackToTop"
-              />
-            </div>
-          </a>
+          <BackToTopLink />
         </div>
       </div>
     </footer>
