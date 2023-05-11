d0a97dca7655dde4c02c87772806191c105f96ed
andrej <andersontwentytwo@gmail.com>
2023-05-11 11:36:51 +0300

    managed hover effects on button inside scss file, structured scss file

--- src/components/Footer/BackToTopButton/BackToTopButton.tsx
+++ src/components/Footer/BackToTopButton/BackToTopButton.tsx
@@ -1,19 +1,7 @@
 import backToTop from '@assets/icons/icon-backToTop.svg';
-import classNames from 'classnames';
-import { useState } from 'react';
 import styles from '../Footer.module.scss';
 
 export const BackToTopButton = () => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
-
   const handleScrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -22,25 +10,16 @@ export const BackToTopButton = () => {
   };
 
   return (
-    <a
-      className={styles.footer_nav}
-      onClick={handleScrollToTop}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
+    <button className={styles.footer_nav} onClick={handleScrollToTop}>
       <p className={styles.footer_nav_text}>Back to top</p>
 
-      <div
-        className={classNames(styles.footer_nav_icon_container, {
-          [styles.footer_nav_icon_container_active]: isHovered,
-        })}
-      >
+      <div className={styles.footer_nav_icon_container}>
         <img
           className={styles.footer_nav_icon}
           src={backToTop}
           alt="BackToTop"
         />
       </div>
-    </a>
+    </button>
   );
 };
