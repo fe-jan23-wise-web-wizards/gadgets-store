128f0d24695d3fd6e7b39c1c6eb247f0e4ccac7f
Vladyslav Mosor <mosorvlad@gmail.com>
2023-05-11 09:59:06 +0200

    404 page bugfix

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -5,26 +5,28 @@ import errorImage from '@assets/error-image.svg';
 
 export const NotFoundPage = () => {
   return (
-    <div className={styles.page}>
-      <div className={styles.page_message}>
-        <p className={styles.page_message_code}>404</p>
+    <div className={styles.wrapper}>
+      <div className={styles.page}>
+        <div className={styles.page_message}>
+          <p className={styles.page_message_code}>404</p>
 
-        <p className={styles.page_message_status}>Page not found</p>
+          <p className={styles.page_message_status}>Page not found</p>
 
-        <p className={styles.page_message_description}>
-          You've clicked on a bad link or entered an invalid URL
-        </p>
+          <p className={styles.page_message_description}>
+            You've clicked on a bad link or entered an invalid URL
+          </p>
 
-        <Link className={styles.page_message_homelink} to="/">
-          Go back to home page
-        </Link>
+          <Link className={styles.page_message_homelink} to="/">
+            Go back to home page
+          </Link>
+        </div>
+
+        <img
+          className={styles.page_image}
+          src={errorImage}
+          alt="Error 404 image"
+        />
       </div>
-
-      <img
-        className={styles.page_image}
-        src={errorImage}
-        alt="Error 404 image"
-      />
     </div>
   );
 };
